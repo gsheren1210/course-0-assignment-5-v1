42d11c5a559601cf1059a013ec0a6ee5a662e5e3
gsheren1210 <63162654+gsheren1210@users.noreply.github.com>
2020-05-01 08:33:12 +0000

    Saving progress for all questions.

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -14,20 +14,26 @@ float CostOfIngredient(const string ingredient) {
     cout << "What is the price of an " << ingredient << endl;
     float price;
     cin >> price;
-    float total = price * NumberOfIngredient();
-    return total;
- }
+    return price;
+}
 
 int main() {
   string avocado = "avocado";
-  float total_avocado = CostOfIngredient(avocado);
-  string tomato = "tomato";
-  float total_tomato = CostOfIngredient(tomato);
-  string onion = "onions";
-  float total_onion = CostOfIngredient(onion);
-  string jalapeno_peppers = "jalapeno peppers";
-  float total_jalapeno = CostOfIngredient(jalapeno_peppers);
+  int num_avocados = NumberOfIngredient(avocado);
+  float total_avocado = CostOfIngredient(avocado) * num_avocados;
 
-  float total_cost = total_avocado
+  string tomato = "tomato";
+  int num_tomato = NumberOfIngredient(tomato);
+  float total_tomato = CostOfIngredient(tomato) * num_tomato;
+
+  string onion = "onions";
+  int num_onion = NumberOfIngredient(onion);
+  float total_onion = CostOfIngredient(onion) * num_onion;
+
+  string jalapeno_peppers = "jalapeno peppers";
+  int num_jalapeno_peppers = NumberOfIngredient(jalapeno_peppers);
+  float total_jalapeno = CostOfIngredient(jalapeno_peppers) * num_jalapeno_peppers;
+
+  float total_cost = total_avocado + total_tomato + total_onion + total_jalapeno;
   cout << "The total cost of the recipe will be: $" << total_cost << endl;
 }
