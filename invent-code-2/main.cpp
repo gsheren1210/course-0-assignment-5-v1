f499be34fe3572fd53511d9e1c4334012ea2ad75
gsheren1210 <63162654+gsheren1210@users.noreply.github.com>
2020-05-02 07:42:01 +0000

    Saving progress for all questions.

--- invent-code-2/main.cpp
+++ invent-code-2/main.cpp
@@ -2,27 +2,64 @@
 #include <string>
 using namespace std;
 
-int main() {
-  int first_number = GetInputNumber();
-  char operator = GetInputOperator();
-  int second_number = GetInputNumber();
+int GetInputNumber(){
+  cout << "Enter an interger: ";
+  int number;
+  cin >> number;
+  return number;
+}
 
-  int running_total = 0;
+string GetInputOperator(){
+  cout << "Enter an operator: ";
+  char op;
+  cin >> op;
+  return op;
+}
+
+int RunningTotal(){
   if (operator == '+') {
-    running_total = first_number + second_number;
+    running_total = running_total + second_number;
   }
   if (operator == '%') {
-    running_total = first_number % second_number;
+    running_total = running_total % next_number;
   }
-  cout << " = " << running_total << endl;
-
-  operator = GetInputOperator();
-  int third_number = GetInputNumber();
   if (operator == '*') {
-    running_total = running_total * third_number;
+    running_total = running_total * next_number;
   }
   if (operator == '/') {
-    running_total = running_total / third_number;
+    running_total = running_total / next_number;
   }
+  return running_total;
+}
+
+int main() {
+  int running_total = 0;
+  int first_number = GetInputNumber();
+  char operator = GetInputOperator();
+  running_total = RunningTotal(operator);
+
+  int second_number = GetInputNumber();
+  char operator = GetInputOperator(); 
+  running_total = RunningTotal(operator); 
+
+  int third_number = GetInputNumber();
+  char operator = GetInputOperator();  
+  running_total = RunningTotal(operator);
+
+  int fourth_number = GetInputNumber();
+  char operator = GetInputOperator();  
+  running_total = RunningTotal(operator);
+
+  int fifth_number = GetInputNumber();
+  char operator = GetInputOperator();
+  running_total = RunningTotal(operator);
+
+  int sixth_number = GetInputNumber();
+  char operator = GetInputOperator();
+  running_total = RunningTotal(operator);
+
+  int seventh _number = GetInputNumber();
+  running_total = RunningTotal(operator);
+
   cout << " = " << running_total << endl;
 }
